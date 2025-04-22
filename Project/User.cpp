fc2e68d95a2ab713883cdc91a52dc5497b6ad4f4
Omar Khairy <67693740+omarkhairy429@users.noreply.github.com>
2025-04-22 18:51:15 +0200

    Update User.cpp

--- Project/User.cpp
+++ Project/User.cpp
@@ -1,20 +1,10 @@
 #include "User.hpp"
 
-string User:: getUserName() const {
-    return userName;
-}
 
-string User:: getPassword() const {
-    return password; 
-}
-        
-void User::setUserName(string userName) {
-    this->userName  = userName;
-}
 
-void User:: setPassword(string password) {
-    this->password = password;
-}
+
+
+
 
 
 
